17d8e186a4393ce54e833c085431b79fe406cd04
Jackson Taylor <jtaylor@e1z2r7p11.42.us.org>
2019-04-24 16:08:42 -0700

    author

---
+++ includes/ft_ls.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtaylor <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/24 14:04:32 by jtaylor           #+#    #+#             */
+/*   Updated: 2019/04/24 16:07:34 by jtaylor          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LS_H
+# define FT_LS_H
+
+# include <dirent.h>
+# include <sys/stats.h>
+# include <sys/types.h>
+# include <sys/xattr.h>
+# include <sys/acl.h>
+# include <time.h>
+# include <pwd.h>
+# include <grp.h>
+# include "libft.h"
+# include "ft_printf.h"
+
+# define FT_MALLOC_ERROR_CODE 1
+typedef struct			s_ft_ls
+{
+
+}						t_ft_ls;
+
+typedef struct			s_ft_ls_dir_info
+{
+	
+}						t_ft_ls_dir_info;
+
+typedef struct			t_ft_ls_base_info;
+{
+
+}						t_ft_ls_base_info;
+#endif
